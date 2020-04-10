05659d67e4c2aaeaf468336c81c512acf235383e
rinnv <rinnv@vnext.com.vn>
2020-04-10 17:33:46 +0700

    code content

--- wp-content/themes/them_demo/common.js
+++ wp-content/themes/them_demo/common.js
@@ -8,6 +8,37 @@ window.onscroll = function() {
         nav.classList.remove("scrolled");
     }
 }
+//slick
+  jQuery(document).ready(function(){
+    jQuery('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      });
+  });
+  // slick multiple
+  jQuery(document).ready(function(){
+    jQuery('.slider-multiple').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      });
+  });
+  // change image
+  jQuery(document).ready(function(){
+    jQuery(".change-image img").click(function(){
+        // Change src attribute of image
+        // var title=jQuery(this).attr('data-header');
+        // alert(title);
+        var data_header= jQuery(".change-image img").parent().parent().parent().next().children().attr('data-header');
+
+       var data_content= jQuery(".change-image img").parent().parent().parent().next().children().attr('data-content');
+       alert(data_header);
+        var change=jQuery(this).attr('src');
+        jQuery(".slider .slick-active img").attr('srcset',change);
+    });    
+});
+
 function changeImage(id){
     let imagePath= document.getElementById(id).getAttribute("src");
     document.getElementById('mainImage').setAttribute('src',imagePath);
--- wp-content/themes/them_demo/index.php
+++ wp-content/themes/them_demo/index.php
@@ -1,9 +1,25 @@
 <?php get_header(); ?>
+<div class="row mb-2">
+    <div class="col-md-12">
+        <h1 class="text-center"> IN THE KNOW </h1>
+    </div>
+</div>
+<div class="slider">
 <?php 
-          if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-<?php get_template_part('content', get_post_format());?>
+if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+<?php get_template_part('content')?>
 <?php endwhile; ?>
 <?php else : ?>
 <?php get_template_part('content','none') ?>
 <?php endif; ?>
+</div>
+<div class="slider-multiple">
+<?php
+if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+<?php get_template_part('content','slider')?>
+<?php endwhile; ?>
+<?php else : ?>
+<?php get_template_part('content','none') ?>
+<?php endif; ?>
+</div>
 <?php get_footer();?>
